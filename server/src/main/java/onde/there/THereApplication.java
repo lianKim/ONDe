77df4b27086c86ea1af21071973cec8e26be1400
kimjungmin <jungmini0601@gmail.com>
2022-10-11 22:25:15 +0900

    백엔드 프로젝트 세팅

---
+++ server/src/main/java/onde/there/THereApplication.java
@@ -0,0 +1,13 @@
+package onde.there;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class THereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(THereApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/onde/there/THereApplicationTests.java
@@ -0,0 +1,13 @@
+package onde.there;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class THereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
