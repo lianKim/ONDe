48d0facbd031ae803c9aefd692818f7f09c4f01d
IAN <ooliskoo@naver.com>
2022-10-12 21:16:31 +0900

    Fix : 이미지 관련 예외처리 추가

--- server/src/main/java/onde/there/image/controller/AmazonS3Controller.java
+++ server/src/main/java/onde/there/image/controller/AmazonS3Controller.java
@@ -22,31 +22,19 @@ public class AmazonS3Controller {
 
 	private final AwsS3Service awsS3Service;
 
-	/**
-	 * Amazon S3에 파일 업로드
-	 *
-	 * @return 성공 시 200 Success와 함께 업로드 된 파일의 파일명 리스트 반환
-	 */
 	@Operation(summary = "Amazon S3에 파일 업로드", description = "Amazon S3에 파일 업로드 ")
 	@PostMapping("/file")
-	public ResponseEntity<List<String>> uploadFile(
-		@Parameter(description = "파일들(여러 파일 업로드 가능)", required = true) @RequestPart List<MultipartFile> multipartFile) {
-		//return ApiResponse.success(awsS3Service.uploadFile(multipartFile));
+	public ResponseEntity<List<String>> uploadFiles(
+		@Parameter(description = "Image (여러 파일 업로드 가능)", required = true) @RequestPart List<MultipartFile> multipartFile) {
 
-		return ResponseEntity.ok(awsS3Service.uploadFile(multipartFile));
+		return ResponseEntity.ok(awsS3Service.uploadFiles(multipartFile));
 	}
 
-	/**
-	 * Amazon S3에 업로드 된 파일을 삭제
-	 *
-	 * @return 성공 시 200 Success
-	 */
 	@Operation(summary = "Amazon S3에 업로드 된 파일을 삭제", description = "Amazon S3에 업로드된 파일 삭제")
 	@DeleteMapping("/file")
 	public ResponseEntity<Void> deleteFile(
 		@Parameter(description = "파일 하나 삭제", required = true) @RequestParam String fileName) {
 		awsS3Service.deleteFile(fileName);
-		//return ApiResponse.success(null);
 		return ResponseEntity.ok().build();
 	}
 
--- server/src/main/java/onde/there/image/service/AwsS3Service.java
+++ server/src/main/java/onde/there/image/service/AwsS3Service.java
@@ -11,12 +11,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AwsS3Service {
@@ -25,11 +27,12 @@ public class AwsS3Service {
 	@Value("${cloud.aws.s3.bucket}")
 	private String bucket;
 
-	public List<String> uploadFile(List<MultipartFile> multipartFile) {
+	public List<String> uploadFiles(List<MultipartFile> multipartFiles) {
 		List<String> fileNameList = new ArrayList<>();
-
-		// forEach 구문을 통해 multipartFile로 넘어온 파일들 하나씩 fileNameList에 추가
-		multipartFile.forEach(file -> {
+		if (multipartFiles.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Multipart file is empty");
+		}
+		multipartFiles.forEach(file -> {
 			String fileName = createFileName(file.getOriginalFilename());
 			ObjectMetadata objectMetadata = new ObjectMetadata();
 			objectMetadata.setContentLength(file.getSize());
@@ -54,14 +57,18 @@ public class AwsS3Service {
 		amazonS3.deleteObject(new DeleteObjectRequest(bucket, fileName));
 	}
 
-	private String createFileName(String fileName) { // 먼저 파일 업로드 시, 파일명을 난수화하기 위해 random으로 돌립니다.
+	private String createFileName(String fileName) {
 		return UUID.randomUUID().toString().concat(getFileExtension(fileName));
 	}
 
-	private String getFileExtension(
-		String fileName) { // file 형식이 잘못된 경우를 확인하기 위해 만들어진 로직이며, 파일 타입과 상관없이 업로드할 수 있게 하기 위해 .의 존재 유무만 판단하였습니다.
+	private String getFileExtension(String fileName) {
 		try {
-			return fileName.substring(fileName.lastIndexOf("."));
+			String extension = fileName.substring(fileName.lastIndexOf("."));
+			if (extension.equals(".png") || extension.equals(".jpg")) {
+				return extension;
+			}
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+				fileName + " 이미지 형식의 파일이 아닙니다.");
 		} catch (StringIndexOutOfBoundsException e) {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
 				"잘못된 형식의 파일(" + fileName + ") 입니다.");
