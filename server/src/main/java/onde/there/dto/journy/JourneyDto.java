ab5743b7c89de43af432574d50e89f773e9714f0
Juwon <tjwndnjs1998@gmail.com>
2022-10-15 01:54:01 +0900

    Feat : JourneyDto 생성 여정을 생성하는 CreateRequest, CreatResponse 생성

--- server/src/main/java/onde/there/dto/journy/JourneyDto.java
+++ server/src/main/java/onde/there/dto/journy/JourneyDto.java
@@ -1,37 +1,76 @@
 package onde.there.dto.journy;
 
 import java.time.LocalDate;
+import java.util.List;
+import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import onde.there.domain.Journey;
+import onde.there.domain.type.JourneyThemeType;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class JourneyDto {
 
-	private long journeyId;
-	private long memberId;
-	private String title;
-	private LocalDate startDay;
-	private LocalDate endDay;
-	private String placeThumbnail;
-	private String disclosure;
+	@Setter
+	@Getter
+	@AllArgsConstructor
+	@NoArgsConstructor
+	@Builder
+	public static class CreateRequest {
 
-	public static JourneyDto fromEntity(Journey journey) {
-		return JourneyDto.builder()
-			.journeyId(journey.getId())
-			.memberId(journey.getMember().getId())
-			.title(journey.getTitle())
-			.startDay(journey.getStartDay())
-			.endDay(journey.getEndDay())
-			.placeThumbnail(journey.getPlaceThumbnail())
-			.disclosure(journey.getDisclosure())
-			.build();
+		@NotBlank(message = "이메일을 입력해주세요.")
+		private String memberEmail;
+
+		@NotBlank(message = "제목을 입력해주세요.")
+		private String title;
+
+		@NotBlank(message = "여정 시작 날짜를 입력해주세요.")
+		@DateTimeFormat(iso = ISO.DATE)
+		private LocalDate startDay;
+
+		@NotBlank(message = "여정 종료 날짜를 입력해주세요.")
+		@DateTimeFormat(iso = ISO.DATE)
+		private LocalDate endDay;
+
+		private String placeThumbnailUrl;
+
+		private List<String> journeyThemeTypes;
+	}
+
+	@Setter
+	@Getter
+	@AllArgsConstructor
+	@NoArgsConstructor
+	@Builder
+	public static class CreateResponse {
+
+		private long journeyId;
+		private long memberId;
+		private String title;
+		private LocalDate startDay;
+		private LocalDate endDay;
+		private String placeThumbnail;
+		private String disclosure;
+		private List<String> journeyThemes;
+
+		public static JourneyDto.CreateResponse fromEntity(Journey journey,
+			List<String> journeyThemes) {
+			return JourneyDto.CreateResponse.builder()
+				.journeyId(journey.getId())
+				.memberId(journey.getMember().getId())
+				.title(journey.getTitle())
+				.startDay(journey.getStartDay())
+				.endDay(journey.getEndDay())
+				.placeThumbnail(journey.getPlaceThumbnailUrl())
+				.disclosure(journey.getDisclosure())
+				.journeyThemes(journeyThemes)
+				.build();
+		}
 	}
 }
