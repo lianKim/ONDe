6e3f77df556ee56cd4304b47d4cef37a16ab5026
kimjungmin <jungmini0601@gmail.com>
2022-10-15 11:31:11 +0900

    Feat: 아이디 중복 확인 추가
    아이디 값을 받아서 중복확인 결과를 알려주는 기능입니다.
    
    GlobalExceptionHandler에 Validation에러 발생시 생기는
    MethodArgumentNotValidException 처리 추가 했습니다.
    
    입력값에 이상이 있을 경우 사용하기 위해서 ErroCode에 BAD_REQUEST 추가 했습니다.

---
+++ server/src/main/java/onde/there/dto/member/MemberDto.java
@@ -0,0 +1,38 @@
+package onde.there.dto.member;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+public class MemberDto {
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Schema(name = "CheckIdRequest", description = "아이디 중복 확인 요청")
+    public static class CheckIdRequest {
+        @NotBlank(message = "아이디를 입력 해 주세요!")
+        @Schema(description = "아이디")
+        private String id;
+
+        @Override
+        public String toString() {
+            return "이메일 중복 확인 요청 { id: " + id + " }";
+        }
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class CheckIdResponse {
+        @Schema(description = "중복 확인 결과")
+        boolean result;
+
+        @Override
+        public String toString() {
+            return "이메일 중복 확인 요청 { result: " + result + " }";
+        }
+    }
+}
