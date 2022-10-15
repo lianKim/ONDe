6bad9a9ab28465865d27825ae328075224fae5e8
roomdoor <roomdoor.lee@gmail.com>
2022-10-15 23:39:05 +0900

    Feat : PlaceException.class 추가, ErrorCode 추가
    Place 관련 에러 발생시 사용할 PlaceException 추가
    ErroCode에
    NOT_FOUND_JOURNEY("존재하지 않는 여정입니다.")
    NOT_FOUND_PLACE("존재하지 않는 장소입니다.")
    MISMATCH_PLACE_CATEGORY_TYPE("일치하는 카테고리 타입이 없습니다.")
    추가
    # <타입> : <제목> 형식으로 작성하며 제목은 최대 50글자 정도로만 입력
    # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성
    
    ################
    # 본문(추가 설명)을 아랫줄에 작성
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (관련된 이슈 번호 등 추가)
    
    ################
    # Feat     : 새로운 기능 추가
    # Fix      : 버그 수정
    # Docs     : 문서 수정
    # Test     : 테스트 코드 추가
    # Refactor : 코드 리팩토링, 파일 혹은 폴더명을 수정하거나 옮기는 작업만인 경우, 파일을 삭제하는 작업만 수행한 경우
    # Design   : CSS 및 레이아웃 작업
    # Chore    : 코드 외 빌드 부분 혹은 패키지 매니저 수정사항
    ################

--- server/src/main/java/onde/there/exception/type/ErrorCode.java
+++ server/src/main/java/onde/there/exception/type/ErrorCode.java
@@ -6,8 +6,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+	NOT_FOUND_JOURNEY("존재하지 않는 여정입니다."),
+	NOT_FOUND_PLACE("존재하지 않는 장소입니다."),
 
-    ;
+	MISMATCH_PLACE_CATEGORY_TYPE("일치하는 카테고리 타입이 없습니다.");
 
-    private final String description;
+	private final String description;
 }
