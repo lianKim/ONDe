44d809730dba240fd84ad6c1ed37c9319b028d6c
Juwon <tjwndnjs1998@gmail.com>
2022-10-16 07:52:19 +0900

    Feat : JourneyService 생성 createJourney 생성 여정 종료 날짜가 시작 날짜 보다 과거일 떄 DATE_ERROR 반환

--- server/src/main/java/onde/there/journey/service/JourneyService.java
+++ server/src/main/java/onde/there/journey/service/JourneyService.java
@@ -1,7 +1,19 @@
 package onde.there.journey.service;
 
+import static onde.there.exception.type.ErrorCode.DATE_ERROR;
+
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import onde.there.domain.Journey;
+import onde.there.domain.JourneyTheme;
+import onde.there.domain.Member;
+import onde.there.domain.type.JourneyThemeType;
+import onde.there.dto.journy.JourneyDto;
+import onde.there.exception.JourneyException;
+import onde.there.journey.repository.JourneyRepository;
+import onde.there.journey.repository.JourneyThemeRepository;
+import onde.there.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,13 +21,43 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class JourneyService {
 
-//	private final JourneyRepository journeyRepository;
-//	private final MemberRepository memberRepository;
-//
-//	public JourneyDto createJourney(CreateJourney.Request request) {
-//
-//		Member member = memberRepository.findByEmail(request.getMemberEmail())
-//			.orElseThrow(() -> new RuntimeException());
-//
-//		return null;
+	private final JourneyRepository journeyRepository;
+	private final JourneyThemeRepository journeyThemeRepository;
+	private final MemberRepository memberRepository;
+
+	public JourneyDto.CreateResponse createJourney(
+		JourneyDto.CreateRequest request) {
+
+		Member member = memberRepository.findByEmail(request.getMemberEmail())
+			.get();
+
+		if (request.getEndDay().isBefore(request.getStartDay())) {
+			throw new JourneyException(DATE_ERROR);
+		}
+
+		Journey journey = Journey.builder()
+			.member(member)
+			.title(request.getTitle())
+			.startDay(request.getStartDay())
+			.endDay(request.getEndDay())
+			.placeThumbnailUrl(request.getPlaceThumbnailUrl())
+			.disclosure(request.getDisclosure())
+			.introductionText(request.getIntroductionText())
+			.build();
+
+		journeyRepository.save(journey);
+
+		List<String> inputJourneyThemes = request.getJourneyThemes();
+		for (String inputJourneyTheme : inputJourneyThemes) {
+			JourneyTheme journeyTheme = JourneyTheme.builder()
+				.journey(journey)
+				.journeyThemeName(
+					JourneyThemeType.findByTheme(inputJourneyTheme))
+				.build();
+			journeyThemeRepository.save(journeyTheme);
+		}
+
+		return JourneyDto.CreateResponse.fromEntity(journey,
+			inputJourneyThemes);
+	}
 }
