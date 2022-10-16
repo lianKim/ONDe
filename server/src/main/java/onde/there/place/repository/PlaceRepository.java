449d6dbc788a19935d98f5e4a14d6dda85df4873
roomdoor <roomdoor.lee@gmail.com>
2022-10-16 19:07:52 +0900

    Feat : /place/delete-all api 구현
    api에 필요한 ErrorCode(DELETED_NOTHING),
    jpa method(deleteAllByJourneyId)
    추가
    # <타입> : <제목> 형식으로 작성하며 제목은 최대 50글자 정도로만 입력
    # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성
    
    ################
    # 본문(추가 설명)을 아랫줄에 작성
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (관련된 이슈 번호 등 추가)
    
    ################
    # Feat     : 새로운 기능 추가
    # Fix      : 버그 수정
    # Docs     : 문서 수정
    # Test     : 테스트 코드 추가
    # Refactor : 코드 리팩토링, 파일 혹은 폴더명을 수정하거나 옮기는 작업만인 경우, 파일을 삭제하는 작업만 수행한 경우
    # Design   : CSS 및 레이아웃 작업
    # Chore    : 코드 외 빌드 부분 혹은 패키지 매니저 수정사항
    ################

--- server/src/main/java/onde/there/place/repository/PlaceRepository.java
+++ server/src/main/java/onde/there/place/repository/PlaceRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PlaceRepository extends JpaRepository<Place, Long> {
 
+	Integer deleteAllByJourneyId(Long journeyId);
 }
