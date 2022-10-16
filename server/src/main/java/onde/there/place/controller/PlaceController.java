eb9f9851959b51c40c353a3e9d0a6fd5745c6cd7
roomdoor <roomdoor.lee@gmail.com>
2022-10-16 19:08:41 +0900

    Feat : /place/delete-all api 구현
    Api, service 구현
    이미지, 좋아요, 댓글 구현후 추가 작업 필
    # <타입> : <제목> 형식으로 작성하며 제목은 최대 50글자 정도로만 입력
    # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성
    
    ################
    # 본문(추가 설명)을 아랫줄에 작성
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (관련된 이슈 번호 등 추가)
    
    ################
    # Feat     : 새로운 기능 추가
    # Fix      : 버그 수정
    # Docs     : 문서 수정
    # Test     : 테스트 코드 추가
    # Refactor : 코드 리팩토링, 파일 혹은 폴더명을 수정하거나 옮기는 작업만인 경우, 파일을 삭제하는 작업만 수행한 경우
    # Design   : CSS 및 레이아웃 작업
    # Chore    : 코드 외 빌드 부분 혹은 패키지 매니저 수정사항
    ################

--- server/src/main/java/onde/there/place/controller/PlaceController.java
+++ server/src/main/java/onde/there/place/controller/PlaceController.java
@@ -5,6 +5,7 @@ import onde.there.dto.place.PlaceDto;
 import onde.there.dto.place.PlaceDto.Response;
 import onde.there.place.service.PlaceService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,5 +21,10 @@ public class PlaceController {
 	@GetMapping("/get")
 	public ResponseEntity<PlaceDto.Response> getPlace(@RequestParam Long placeId) {
 		return ResponseEntity.ok(Response.toResponse(placeService.getPlace(placeId)));
+	}
+
+	@DeleteMapping("/delete-all")
+	public ResponseEntity<?> deleteAll(@RequestParam Long journeyId) {
+		return ResponseEntity.ok(placeService.deleteAll(journeyId));
 	}
 }
--- server/src/main/java/onde/there/place/service/PlaceService.java
+++ server/src/main/java/onde/there/place/service/PlaceService.java
@@ -1,9 +1,11 @@
 package onde.there.place.service;
 
+import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import onde.there.domain.Place;
 import onde.there.exception.PlaceException;
 import onde.there.exception.type.ErrorCode;
+import onde.there.journey.repository.JourneyRepository;
 import onde.there.place.repository.PlaceRepository;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +15,24 @@ public class PlaceService {
 
 	private final PlaceRepository placeRepository;
 
+	private final JourneyRepository journeyRepository;
 
 	public Place getPlace(Long placeId) {
 		return placeRepository.findById(placeId)
 			.orElseThrow(() -> new PlaceException(ErrorCode.NOT_FOUND_PLACE));
+	}
+
+	@Transactional
+	public boolean deleteAll(Long journeyId) {
+		journeyRepository.findById(journeyId)
+			.orElseThrow(() -> new PlaceException(ErrorCode.NOT_FOUND_JOURNEY));
+		//TODO : 장소에 포함된 모든 댓글 좋아요 이미지 삭제 구현 필요
+
+		Integer result = placeRepository.deleteAllByJourneyId(journeyId);
+		if (result == 0) {
+			throw new PlaceException(ErrorCode.DELETED_NOTING);
+		}
+
+		return true;
 	}
 }
