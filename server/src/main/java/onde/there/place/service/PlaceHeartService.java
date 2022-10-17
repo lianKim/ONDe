271d6bf2af0288552fd2662de092d427356e10fc
roomdoor <roomdoor.lee@gmail.com>
2022-10-17 18:22:38 +0900

    Feat : Place placeHeartSum update method
    좋아요를 눌렀을 때 place의 좋아요가 1000 개 미만일 때
    즉시 Place의 placeHeartSum 갯수 업데이트
    
    좋아요를 눌렀을 때 place의 좋아요가 1000 개 이상일 때
    PlaceHeartSchduling Entity 저장 -> Place 와 OneToOne 관계
    # <타입> : <제목> 형식으로 작성하며 제목은 최대 50글자 정도로만 입력
    # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성
    
    ################
    # 본문(추가 설명)을 아랫줄에 작성
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (관련된 이슈 번호 등 추가)
    
    ################
    # Feat     : 새로운 기능 추가
    # Fix      : 버그 수정
    # Docs     : 문서 수정
    # Test     : 테스트 코드 추가
    # Refactor : 코드 리팩토링, 파일 혹은 폴더명을 수정하거나 옮기는 작업만인 경우, 파일을 삭제하는 작업만 수행한 경우
    # Design   : CSS 및 레이아웃 작업
    # Chore    : 코드 외 빌드 부분 혹은 패키지 매니저 수정사항
    ################

--- server/src/main/java/onde/there/place/service/PlaceHeartService.java
+++ server/src/main/java/onde/there/place/service/PlaceHeartService.java
@@ -4,10 +4,12 @@ import lombok.RequiredArgsConstructor;
 import onde.there.domain.Member;
 import onde.there.domain.Place;
 import onde.there.domain.PlaceHeart;
+import onde.there.domain.PlaceHeartScheduling;
 import onde.there.exception.PlaceException;
 import onde.there.exception.type.ErrorCode;
 import onde.there.member.repository.MemberRepository;
 import onde.there.place.repository.PlaceHeartRepository;
+import onde.there.place.repository.PlaceHeartSchedulingRepository;
 import onde.there.place.repository.PlaceRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +21,7 @@ public class PlaceHeartService {
 	private final PlaceHeartRepository placeHeartRepository;
 	private final PlaceRepository placeRepository;
 	private final MemberRepository memberRepository;
+	private final PlaceHeartSchedulingRepository placeHeartSchedulingRepository;
 
 	@Transactional
 	public boolean heart(Long placeId, String memberId) {
@@ -28,9 +31,10 @@ public class PlaceHeartService {
 			.orElseThrow(() -> new PlaceException(ErrorCode.NOT_FOUND_MEMBER));
 
 		if (place.getPlaceHeartSum() >= 1000) {
-			//TODO 1000 개 넘었을 떄 스케쥴링
+			addSchedule(placeId);
 		} else {
 			place.setPlaceHeartSum(place.getPlaceHeartSum() + 1);
+			placeRepository.save(place);
 		}
 
 		if (placeHeartRepository.existsByPlaceIdAndMemberId(placeId, memberId)) {
@@ -43,5 +47,12 @@ public class PlaceHeartService {
 			.build());
 
 		return true;
+	}
+
+	private void addSchedule(Long placeId) {
+		if (!placeHeartSchedulingRepository.existsByPlaceId(placeId)) {
+			placeHeartSchedulingRepository.save(PlaceHeartScheduling
+				.builder().build());
+		}
 	}
 }
