9f6f02fe34b0b0f84c1f25fae05af15df4a03212
Juwon <tjwndnjs1998@gmail.com>
2022-10-18 00:13:00 +0900

    Feat : JourneyThemeRepository 수정 @Repository annotation 추가

--- server/src/main/java/onde/there/journey/repository/JourneyThemeRepository.java
+++ server/src/main/java/onde/there/journey/repository/JourneyThemeRepository.java
@@ -2,7 +2,9 @@ package onde.there.journey.repository;
 
 import onde.there.domain.JourneyTheme;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JourneyThemeRepository extends
 	JpaRepository<JourneyTheme, Long> {
 
