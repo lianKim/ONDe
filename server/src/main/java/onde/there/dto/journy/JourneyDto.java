1090d103a9f146065f1850a43ebf84e9cf625e03
Juwon <tjwndnjs1998@gmail.com>
2022-10-18 00:10:40 +0900

    Feat : JourneyDto 수정 여정 생성에 필요한 인원수, regionGroups 추가, annotation 수정

--- server/src/main/java/onde/there/dto/journy/JourneyDto.java
+++ server/src/main/java/onde/there/dto/journy/JourneyDto.java
@@ -2,15 +2,15 @@ package onde.there.dto.journy;
 
 import java.time.LocalDate;
 import java.util.List;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Null;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import onde.there.domain.Journey;
-import onde.there.domain.type.JourneyThemeType;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 
@@ -32,19 +32,29 @@ public class JourneyDto {
 		private String title;
 
 		@DateTimeFormat(iso = ISO.DATE)
-		private LocalDate startDay;
+		private LocalDate startDate;
 
 		@DateTimeFormat(iso = ISO.DATE)
-		private LocalDate endDay;
+		private LocalDate endDate;
+
+		@NotNull
+		@Min(1)
+		private int numberOfPeople;
 
 		@NotBlank(message = "공개범위를 선택해주세요.")
 		private String disclosure;
 
-		private String placeThumbnailUrl;
 
+		@NotNull
 		private List<String> journeyThemes;
 
+		@NotNull
+		private List<RegionGroup> regionGroups;
+
 		private String introductionText;
+
+		private String placeThumbnailUrl;
+
 	}
 
 	@Setter
@@ -57,26 +67,38 @@ public class JourneyDto {
 		private long journeyId;
 		private String memberId;
 		private String title;
-		private LocalDate startDay;
-		private LocalDate endDay;
-		private String placeThumbnailUrl;
+		private LocalDate startDate;
+		private LocalDate endDate;
+		private int numberOfPeople;
 		private String disclosure;
 		private List<String> journeyThemes;
+		private List<RegionGroup> regionGroups;
 		private String introductionText;
+		private String placeThumbnailUrl;
+
 
 		public static JourneyDto.CreateResponse fromEntity(Journey journey,
-			List<String> journeyThemes) {
-			return JourneyDto.CreateResponse.builder()
+			List<String> journeyThemes, List<RegionGroup> regionGroups) {
+			return CreateResponse.builder()
 				.journeyId(journey.getId())
 				.memberId(journey.getMember().getId())
 				.title(journey.getTitle())
-				.startDay(journey.getStartDay())
-				.endDay(journey.getEndDay())
+				.startDate(journey.getStartDate())
+				.endDate(journey.getEndDate())
+				.numberOfPeople(journey.getNumberOfPeople())
 				.placeThumbnailUrl(journey.getPlaceThumbnailUrl())
 				.disclosure(journey.getDisclosure())
 				.journeyThemes(journeyThemes)
 				.introductionText(journey.getIntroductionText())
+				.regionGroups(regionGroups)
 				.build();
 		}
+	}
+
+	@Getter
+	public static class RegionGroup {
+
+		String area;
+		List<String> regions;
 	}
 }
