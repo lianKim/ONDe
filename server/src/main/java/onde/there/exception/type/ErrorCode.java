ca65114d907de7fb431ea334a727c68a2d64367b
roomdoor <roomdoor.lee@gmail.com>
2022-10-17 16:50:25 +0900

    Feat : /place-heart/heart api ErrorCode case
    ErrorCode case 추가
    NOT_FOUND_MEMBER
    ALREADY_HEARTED
    # <타입> : <제목> 형식으로 작성하며 제목은 최대 50글자 정도로만 입력
    # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성
    
    ################
    # 본문(추가 설명)을 아랫줄에 작성
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (관련된 이슈 번호 등 추가)
    
    ################
    # Feat     : 새로운 기능 추가
    # Fix      : 버그 수정
    # Docs     : 문서 수정
    # Test     : 테스트 코드 추가
    # Refactor : 코드 리팩토링, 파일 혹은 폴더명을 수정하거나 옮기는 작업만인 경우, 파일을 삭제하는 작업만 수행한 경우
    # Design   : CSS 및 레이아웃 작업
    # Chore    : 코드 외 빌드 부분 혹은 패키지 매니저 수정사항
    ################

--- server/src/main/java/onde/there/exception/type/ErrorCode.java
+++ server/src/main/java/onde/there/exception/type/ErrorCode.java
@@ -8,9 +8,12 @@ import lombok.Getter;
 public enum ErrorCode {
 	NOT_FOUND_JOURNEY("존재하지 않는 여정입니다."),
 	NOT_FOUND_PLACE("존재하지 않는 장소입니다."),
+	NOT_FOUND_MEMBER("존재하지 않는 유저입니다."),
+
 
 	MISMATCH_PLACE_CATEGORY_TYPE("일치하는 카테고리 타입이 없습니다."),
 	BAD_REQUEST("입력 값을 확인해 주세요!"),
+	ALREADY_HEARTED("이미 좋아요가 눌린 상태입니다.")
 
 	;
 	private final String description;
