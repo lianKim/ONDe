bfb4331e2afa2017b620fb4443a0267c22833fe6
roomdoor <roomdoor.lee@gmail.com>
2022-10-17 16:49:35 +0900

    Feat : /place-heart/heart api Service
    PlaceHeartService -> heart 메소드 작성
    장소와 유저가 존재하는지 확인
    장소에 좋아요가 1000개 이상일 경우 스케쥴링으로 처리
    좋아요를 누른 유저인지 확인
    # <타입> : <제목> 형식으로 작성하며 제목은 최대 50글자 정도로만 입력
    # 제목을 아랫줄에 작성, 제목 끝에 마침표 금지, 무엇을 했는지 명확하게 작성
    
    ################
    # 본문(추가 설명)을 아랫줄에 작성
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (관련된 이슈 번호 등 추가)
    
    ################
    # Feat     : 새로운 기능 추가
    # Fix      : 버그 수정
    # Docs     : 문서 수정
    # Test     : 테스트 코드 추가
    # Refactor : 코드 리팩토링, 파일 혹은 폴더명을 수정하거나 옮기는 작업만인 경우, 파일을 삭제하는 작업만 수행한 경우
    # Design   : CSS 및 레이아웃 작업
    # Chore    : 코드 외 빌드 부분 혹은 패키지 매니저 수정사항
    ################

--- server/src/main/java/onde/there/place/service/PlaceHeartService.java
+++ server/src/main/java/onde/there/place/service/PlaceHeartService.java
@@ -1,11 +1,47 @@
 package onde.there.place.service;
 
 import lombok.RequiredArgsConstructor;
+import onde.there.domain.Member;
+import onde.there.domain.Place;
+import onde.there.domain.PlaceHeart;
+import onde.there.exception.PlaceException;
+import onde.there.exception.type.ErrorCode;
+import onde.there.member.repository.MemberRepository;
 import onde.there.place.repository.PlaceHeartRepository;
+import onde.there.place.repository.PlaceRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class PlaceHeartService {
+
 	private final PlaceHeartRepository placeHeartRepository;
+	private final PlaceRepository placeRepository;
+	private final MemberRepository memberRepository;
+
+	@Transactional
+	public boolean heart(Long placeId, String memberId) {
+		Place place = placeRepository.findById(placeId)
+			.orElseThrow(() -> new PlaceException(ErrorCode.NOT_FOUND_PLACE));
+		Member member = memberRepository.findById(memberId)
+			.orElseThrow(() -> new PlaceException(ErrorCode.NOT_FOUND_MEMBER));
+
+		if (place.getPlaceHeartSum() >= 1000) {
+			//TODO 1000 개 넘었을 떄 스케쥴링
+		} else {
+			place.setPlaceHeartSum(place.getPlaceHeartSum() + 1);
+		}
+
+		if (placeHeartRepository.existsByPlaceIdAndMemberId(placeId, memberId)) {
+			throw new PlaceException(ErrorCode.ALREADY_HEARTED);
+		}
+
+		placeHeartRepository.save(PlaceHeart.builder()
+			.place(place)
+			.member(member)
+			.build());
+
+		return true;
+	}
 }
