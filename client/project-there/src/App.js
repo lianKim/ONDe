d0a6860765df1fb13da1ef9d56c030268df6f776
lianKim <5ffcut@gmail.com>
2022-10-17 11:58:45 +0900

    Design: Global Style 추가

--- client/project-there/src/App.js
+++ client/project-there/src/App.js
@@ -1,7 +1,7 @@
 import { React, Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Reset } from 'styled-reset';
-import GlobalStyle from './styles/GlobalStyle';
+import GlobalStyle from './styles/global';
 
 const LayoutPage = lazy(() => import('./pages/LayoutPage'));
 const MainPage = lazy(() => import('./pages/MainPage'));
