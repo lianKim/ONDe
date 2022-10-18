7d3607eb67f56ea79e0af3fb78ec739f7ccd5b3f
IAN <ooliskoo@naver.com>
2022-10-18 20:37:19 +0900

    Feat : 댓글 crud 테스트 케이스

--- server/src/test/java/onde/there/comment/service/CommentServiceTest.java
+++ server/src/test/java/onde/there/comment/service/CommentServiceTest.java
@@ -1,7 +1,11 @@
 package onde.there.comment.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.ArrayList;
+import java.util.List;
 import onde.there.comment.exception.CommentErrorCode;
 import onde.there.comment.exception.CommentException;
 import onde.there.comment.repository.CommentRepository;
@@ -9,6 +13,7 @@ import onde.there.domain.Comment;
 import onde.there.domain.Member;
 import onde.there.domain.Place;
 import onde.there.dto.comment.CommentDto;
+import onde.there.dto.comment.CommentDto.Response;
 import onde.there.member.repository.MemberRepository;
 import onde.there.place.repository.PlaceRepository;
 import org.junit.jupiter.api.Test;
@@ -31,8 +36,8 @@ class CommentServiceTest {
 
 	@Test
 	void 댓글_생성() {
-	    //given
-		Member member = memberRepository.save(new Member("asd", "", "",""));
+		//given
+		Member member = memberRepository.save(new Member("asd", "", "", ""));
 		Place place = placeRepository.save(new Place());
 		CommentDto.CreateRequest request =
 			CommentDto.CreateRequest.builder()
@@ -40,9 +45,9 @@ class CommentServiceTest {
 				.placeId(place.getId())
 				.comment("댓글입니다.")
 				.build();
-	    //when
+		//when
 		Comment comment = commentService.createComment(request);
-	    //then
+		//then
 		assertEquals(member.getId(), comment.getMember().getId());
 		assertEquals(place.getId(), comment.getPlace().getId());
 		assertEquals(request.getComment(), comment.getComment());
@@ -51,7 +56,7 @@ class CommentServiceTest {
 	@Test
 	void 댓글_생성_에러_멤버아이디() {
 		//given
-		Member member = memberRepository.save(new Member("1", "", "",""));
+		Member member = memberRepository.save(new Member("1", "", "", ""));
 		Place place = placeRepository.save(new Place());
 		CommentDto.CreateRequest request =
 			CommentDto.CreateRequest.builder()
@@ -60,7 +65,8 @@ class CommentServiceTest {
 				.comment("댓글입니다.")
 				.build();
 		//when
-		CommentException commentException = assertThrows(CommentException.class, () -> commentService.createComment(request));
+		CommentException commentException = assertThrows(CommentException.class,
+			() -> commentService.createComment(request));
 		//then
 		assertEquals(CommentErrorCode.NOT_FOUND_MEMBER, commentException.getErrorCode());
 	}
@@ -68,17 +74,149 @@ class CommentServiceTest {
 	@Test
 	void 댓글_생성_에러_장소아이디() {
 		//given
-		Member member = memberRepository.save(new Member("1", "", "",""));
+		Member member = memberRepository.save(new Member("1", "", "", ""));
 		Place place = placeRepository.save(new Place());
 		CommentDto.CreateRequest request =
 			CommentDto.CreateRequest.builder()
 				.memberId(member.getId())
-				.placeId(2L)
+				.placeId(123L)
 				.comment("댓글입니다.")
 				.build();
 		//when
-		CommentException commentException = assertThrows(CommentException.class, () -> commentService.createComment(request));
+		CommentException commentException = assertThrows(CommentException.class,
+			() -> commentService.createComment(request));
 		//then
 		assertEquals(CommentErrorCode.NOT_FOUND_PLACE, commentException.getErrorCode());
+	}
+
+	@Test
+	void 댓글_조회() {
+		//given
+		Member member = memberRepository.save(new Member("asd", "", "", ""));
+		Place place = placeRepository.save(new Place());
+		List<Comment> comments = new ArrayList<>();
+		for (int i = 0; i < 3; i++) {
+			String commentText = String.format("%d번째 댓글", i);
+			comments.add(commentService.createComment(CommentDto.CreateRequest.builder()
+				.memberId(member.getId())
+				.placeId(place.getId())
+				.comment(commentText)
+				.build()));
+		}
+		//when
+		List<CommentDto.Response> responses = commentService.getComments(place.getId());
+		//then
+		for (Response response : responses) {
+			System.out.println(response.getComment());
+			assertFalse(comments.contains(response.getComment()));
+		}
+	}
+
+	@Test
+	void 댓글_조회_에러_장소아이디() {
+		//given
+		Member member = memberRepository.save(new Member("asd", "", "", ""));
+		Place place = placeRepository.save(new Place());
+		List<Comment> comments = new ArrayList<>();
+		for (int i = 0; i < 3; i++) {
+			String commentText = String.format("%d번째 댓글", i);
+			comments.add(commentService.createComment(CommentDto.CreateRequest.builder()
+				.memberId(member.getId())
+				.placeId(place.getId())
+				.comment(commentText)
+				.build()));
+		}
+		//when
+		CommentException commentException = assertThrows(CommentException.class,
+			() -> commentService.getComments(123L));
+		//then
+		assertEquals(CommentErrorCode.NOT_FOUND_PLACE, commentException.getErrorCode());
+	}
+
+	@Test
+	void 댓글_수정() {
+		//given
+		Member member = memberRepository.save(new Member("asd", "", "", ""));
+		Place place = placeRepository.save(new Place());
+		CommentDto.CreateRequest request =
+			CommentDto.CreateRequest.builder()
+				.memberId(member.getId())
+				.placeId(place.getId())
+				.comment("댓글입니다.")
+				.build();
+		Comment comment = commentService.createComment(request);
+
+		CommentDto.UpdateRequest updateRequest =
+			CommentDto.UpdateRequest.builder()
+				.commentId(comment.getId())
+				.comment("수정된 댓글입니다.")
+				.build();
+		//when
+		Long id = commentService.updateComment(updateRequest);
+		//then
+		assertEquals(comment.getId(), id);
+		assertEquals(updateRequest.getComment(),
+			commentRepository.findById(id).orElseThrow().getComment());
+	}
+
+	@Test
+	void 댓글_수정_에러_댓글아이디() {
+		//given
+		Member member = memberRepository.save(new Member("asd", "", "", ""));
+		Place place = placeRepository.save(new Place());
+		CommentDto.CreateRequest request =
+			CommentDto.CreateRequest.builder()
+				.memberId(member.getId())
+				.placeId(place.getId())
+				.comment("댓글입니다.")
+				.build();
+		Comment comment = commentService.createComment(request);
+
+		CommentDto.UpdateRequest updateRequest =
+			CommentDto.UpdateRequest.builder()
+				.commentId(123L)
+				.comment("수정된 댓글입니다.")
+				.build();
+		//when
+		CommentException commentException = assertThrows(CommentException.class,
+			() -> commentService.updateComment(updateRequest));
+		//then
+		assertEquals(CommentErrorCode.NOT_FOUND_COMMENT, commentException.getErrorCode());
+	}
+
+	@Test
+	void 댓글_삭제() {
+		//given
+		Member member = memberRepository.save(new Member("asd", "", "", ""));
+		Place place = placeRepository.save(new Place());
+		CommentDto.CreateRequest request =
+			CommentDto.CreateRequest.builder()
+				.memberId(member.getId())
+				.placeId(place.getId())
+				.comment("댓글입니다.")
+				.build();
+		Comment comment = commentService.createComment(request);
+		//when
+		commentService.deleteComment(comment.getId());
+		//then
+		assertEquals(0, commentRepository.count());
+	}
+
+	@Test
+	void 댓글_삭제_에러_댓글아이디() {
+		//given
+		Member member = memberRepository.save(new Member("asd", "", "", ""));
+		Place place = placeRepository.save(new Place());
+		CommentDto.CreateRequest request =
+			CommentDto.CreateRequest.builder()
+				.memberId(member.getId())
+				.placeId(place.getId())
+				.comment("댓글입니다.")
+				.build();
+		Comment comment = commentService.createComment(request);
+		//when
+		CommentException commentException = assertThrows(CommentException.class, () -> commentService.deleteComment(123L));
+		//then
+		assertEquals(CommentErrorCode.NOT_FOUND_COMMENT, commentException.getErrorCode());
 	}
 }
