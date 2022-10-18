07cc141c14a64688f76d0103b8f9a922a54721c2
Juwon <tjwndnjs1998@gmail.com>
2022-10-18 19:34:54 +0900

    Feat : JourneyErrorCode에 ErrorCode추가 들어온 Area가 일치하는 지역이 없는 경우 -> NO_AREA_MATCHES ErrorCode를 추가 하였습니다.

--- server/src/main/java/onde/there/journey/exception/JourneyErrorCode.java
+++ server/src/main/java/onde/there/journey/exception/JourneyErrorCode.java
@@ -9,7 +9,8 @@ public enum JourneyErrorCode {
 	NOT_FOUND_MEMBER("존재하지 않는 유저입니다."),
 	DATE_ERROR("잘못된 날짜입니다."),
 	THERE_IS_NO_MATCHING_THEME("일치하는 테마가 없습니다."),
-	NEED_A_DETAILED_REGION("해당 지역은 추가적인 시/도 정보가 필요합니다.")
+	NEED_A_DETAILED_REGION("해당 지역은 추가적인 시/도 정보가 필요합니다."),
+	NO_AREA_MATCHES("일치하는 지역이 없습니다.")
 	;
 
 
