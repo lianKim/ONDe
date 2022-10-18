1e8bebac61889d4d41c7746653497f455756e8d1
IAN <ooliskoo@naver.com>
2022-10-18 17:04:01 +0900

    Fix : 댓글 api 트랜잭션 추가

--- server/src/main/java/onde/there/comment/controller/CommentController.java
+++ server/src/main/java/onde/there/comment/controller/CommentController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import onde.there.comment.service.CommentService;
 import onde.there.dto.comment.CommentDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,7 @@ public class CommentController {
 
 	private final CommentService commentService;
 
+	@Transactional
 	@Operation(summary = "장소에 댓글 쓰기", description = "장소에 댓글 쓰기")
 	@PostMapping("/place/comment")
 	public ResponseEntity<Long> createComment(
@@ -29,18 +31,21 @@ public class CommentController {
 		return ResponseEntity.ok(commentService.createComment(request).getId());
 	}
 
+	@Transactional(readOnly = true)
 	@Operation(summary = "장소 댓글 조회", description = "장소 댓글 조회")
 	public ResponseEntity<List<CommentDto.Response>> getComments(
 		@RequestParam(value = "id") @Parameter(description = "장소 아이디", required = true) Long placeId) {
 		return ResponseEntity.ok(commentService.getComments(placeId));
 	}
 
+	@Transactional
 	@Operation(summary = "장소 댓글 수정", description = "장소 댓글 수정")
 	public ResponseEntity<Long> updateComment(
 		@RequestBody @Parameter(description = "댓글 업데이트 리퀘스트", required = true) @Valid CommentDto.UpdateRequest request) {
 		return ResponseEntity.ok(commentService.updateComment(request));
 	}
 
+	@Transactional
 	@Operation(summary = "장소 댓글 삭제", description = "장소 댓글 삭제")
 	public ResponseEntity<Void> updateComment(
 		@RequestParam(value = "id") @Parameter(description = "댓글 아이디", required = true) Long commentId) {
