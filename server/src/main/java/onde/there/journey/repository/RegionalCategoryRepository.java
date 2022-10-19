b6dfe0281b8039447975859565b2e91033177ceb
Juwon <tjwndnjs1998@gmail.com>
2022-10-19 22:31:22 +0900

    Feat : journeyList 조회에 관련한 Repository에 메소드 생성 JourneyRepository -> findAllByDisclosure : public의 공개 범위의 Journey를 가져옵니다. JourneyThemeRepository -> findAllByJourneyId : 여정의 id로 JourneyTheme을 가져옵니다. RegionalCategoryRepository -> findAllByJourneyId : 여정의 id로 RegionalCategory를 가져옵니다.

--- server/src/main/java/onde/there/journey/repository/RegionalCategoryRepository.java
+++ server/src/main/java/onde/there/journey/repository/RegionalCategoryRepository.java
@@ -1,5 +1,6 @@
 package onde.there.journey.repository;
 
+import java.util.List;
 import onde.there.domain.RegionalCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,4 +9,5 @@ import org.springframework.stereotype.Repository;
 public interface RegionalCategoryRepository extends
 	JpaRepository<RegionalCategory, Long> {
 
+	List<RegionalCategory> findAllByJourneyId(Long journeyId);
 }
