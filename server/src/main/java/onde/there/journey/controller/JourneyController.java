2f4e295ac279b29d5bdcc6aa2251192b10d5dc84
Juwon <tjwndnjs1998@gmail.com>
2022-10-19 22:29:07 +0900

    Feat : JourneyList, JourneyMyList Controller 생성 여정에 대한 정보, 여정 테마, 지역 카테고리를 List로 반환을 합니다.

--- server/src/main/java/onde/there/journey/controller/JourneyController.java
+++ server/src/main/java/onde/there/journey/controller/JourneyController.java
@@ -1,13 +1,17 @@
 package onde.there.journey.controller;
 
+import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import onde.there.dto.journy.JourneyDto;
+import onde.there.dto.journy.JourneyDto.JourneyListResponse;
 import onde.there.journey.service.JourneyService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,6 +26,19 @@ public class JourneyController {
 		@RequestBody @Valid JourneyDto.CreateRequest request) {
 
 		return ResponseEntity.ok(journeyService.createJourney(request));
+	}
+
+	@GetMapping("/list")
+	public ResponseEntity<List<JourneyListResponse>> getJourneyList() {
+
+		return ResponseEntity.ok(journeyService.list());
+	}
+
+	@GetMapping("/myList")
+	public ResponseEntity<List<JourneyListResponse>> getMyJourneyList(
+		@RequestParam String memberId) {
+
+		return ResponseEntity.ok(journeyService.myList(memberId));
 	}
 
 }
