162c76c1fbd04133d496369a1aae230a059770a1
roomdoor <roomdoor.lee@gmail.com>
2022-10-21 14:31:56 +0900

    Test : /place/update api test
    Controller test
    성공
    -> 업데이트 된 Place 값을 Response 객채로 변환하여 반환
    실패
    1. PlaceException - NOT_FOUND_PLACE
    2. PlaceException - MISMATCH_PLACE_CATEGORY_TYPE
    3. ImageException - FAILED_UPLOAD
    
    Service Test
    성공
    -> 업데이트 된 Place 값을 Response 객채로 변환하여 반환
    실패
    1. PlaceException - NOT_FOUND_PLACE
    2. PlaceException - MISMATCH_PLACE_CATEGORY_TYPE

--- server/src/test/java/onde/there/place/service/PlaceServiceTest.java
+++ server/src/test/java/onde/there/place/service/PlaceServiceTest.java
@@ -4,19 +4,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import onde.there.domain.Journey;
 import onde.there.domain.Place;
+import onde.there.domain.PlaceImage;
+import onde.there.domain.type.PlaceCategoryType;
+import onde.there.dto.place.PlaceDto;
+import onde.there.dto.place.PlaceDto.CreateRequest;
+import onde.there.dto.place.PlaceDto.Response;
+import onde.there.dto.place.PlaceDto.UpdateRequest;
 import onde.there.exception.PlaceException;
 import onde.there.exception.type.ErrorCode;
+import onde.there.image.service.AwsS3Service;
 import onde.there.journey.repository.JourneyRepository;
+import onde.there.place.repository.PlaceImageRepository;
 import onde.there.place.repository.PlaceRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @SpringBootTest
 @Transactional
@@ -29,7 +42,112 @@ class PlaceServiceTest {
 	private PlaceRepository placeRepository;
 
 	@Autowired
+	private PlaceImageRepository placeImageRepository;
+
+	@Autowired
 	private JourneyRepository journeyRepository;
+
+	@Autowired
+	private AwsS3Service awsS3Service;
+
+	@Test
+	void 장소_저장() throws IOException {
+		//given
+		List<MultipartFile> multipartFile = new ArrayList<>();
+		for (int i = 1; i <= 3; i++) {
+			String file = String.format("%d.png", i);
+			FileInputStream fis = new FileInputStream("src/main/resources/testImages/" + file);
+			multipartFile.add(new MockMultipartFile(String.format("%d", i), file, "png", fis));
+		}
+		Journey journey = journeyRepository.save(new Journey());
+		PlaceDto.CreateRequest request = CreateRequest.builder()
+			.latitude(123.0)
+			.longitude(234.0)
+			.title("test")
+			.text("테스트 중 입니다.")
+			.addressName("경기도 김포시 고촌읍 어쩌구 저쩌구")
+			.region1("경기도")
+			.region2("김포시")
+			.region3("고촌읍")
+			.region4("어쩌구 저쩌구")
+			.placeTime(LocalDateTime.now())
+			.journeyId(journey.getId())
+			.placeCategory(PlaceCategoryType.NATURE.getDescription())
+			.placeName("어딘가")
+			.build();
+		//when
+		Place place = placeService.createPlace(multipartFile, request);
+		//then
+		assertEquals(place.getJourney().getId(), journey.getId());
+		assertEquals(3, placeImageRepository.count());
+	}
+
+	@Test
+	void 장소_저장_에러_여행아이디() throws IOException {
+		//given
+		List<MultipartFile> multipartFile = new ArrayList<>();
+		for (int i = 1; i <= 3; i++) {
+			String file = String.format("%d.png", i);
+			FileInputStream fis = new FileInputStream("src/main/resources/testImages/" + file);
+			multipartFile.add(new MockMultipartFile(String.format("%d", i), file, "png", fis));
+		}
+		PlaceDto.CreateRequest request = CreateRequest.builder()
+			.latitude(123.0)
+			.longitude(234.0)
+			.title("test")
+			.text("테스트 중 입니다.")
+			.addressName("경기도 김포시 고촌읍 어쩌구 저쩌구")
+			.region1("경기도")
+			.region2("김포시")
+			.region3("고촌읍")
+			.region4("어쩌구 저쩌구")
+			.placeTime(LocalDateTime.now())
+			.journeyId(1L)
+			.placeCategory(PlaceCategoryType.NATURE.getDescription())
+			.placeName("어딘가")
+			.build();
+		//when
+		//then
+		PlaceException placeException = assertThrows(PlaceException.class,
+			() -> placeService.createPlace(multipartFile, request));
+		assertEquals(ErrorCode.NOT_FOUND_JOURNEY.getDescription(),
+			placeException.getErrorMessage());
+	}
+
+	@Test
+	void 장소_저장_에러_카테고리() throws IOException {
+		//given
+		List<MultipartFile> multipartFile = new ArrayList<>();
+		for (int i = 1; i <= 3; i++) {
+			String file = String.format("%d.png", i);
+			FileInputStream fis = new FileInputStream("src/main/resources/testImages/" + file);
+			multipartFile.add(new MockMultipartFile(String.format("%d", i), file, "png", fis));
+		}
+		Journey journey = journeyRepository.save(new Journey());
+		PlaceDto.CreateRequest request = CreateRequest.builder()
+			.latitude(123.0)
+			.longitude(234.0)
+			.title("test")
+			.text("테스트 중 입니다.")
+			.addressName("경기도 김포시 고촌읍 어쩌구 저쩌구")
+			.region1("경기도")
+			.region2("김포시")
+			.region3("고촌읍")
+			.region4("어쩌구 저쩌구")
+			.placeTime(LocalDateTime.now())
+			.journeyId(journey.getId())
+			.placeCategory("내츄럴")
+			.placeName("어딘가")
+			.build();
+		//when
+		//then
+
+		PlaceException placeException = assertThrows(PlaceException.class,
+			() -> placeService.createPlace(multipartFile, request));
+
+		assertEquals(ErrorCode.MISMATCH_PLACE_CATEGORY_TYPE.getDescription(),
+			placeException.getErrorMessage());
+	}
 
 	@DisplayName("01_00. getPlace success")
 	@Test
@@ -77,7 +195,7 @@ class PlaceServiceTest {
 		}
 
 		//when
-		List<Place> list = placeService.list(1L);
+		List<Place> list = placeService.list(journey.getId());
 
 		//then
 		assertEquals(list.size(), 3);
@@ -172,5 +290,191 @@ class PlaceServiceTest {
 
 		//then
 		assertEquals(placeException.getErrorCode(), ErrorCode.NOT_FOUND_JOURNEY);
+	}
+
+	@DisplayName("05_00. updatePlace success")
+	@Test
+	public void test_05_00() throws IOException {
+		//given
+		Journey saveJourney = journeyRepository.save(Journey.builder().build());
+
+		Place savePlace = placeRepository.save(Place.builder()
+			.latitude(1.0)
+			.longitude(1.0)
+			.title("title test")
+			.text("text test")
+			.addressName("total address")
+			.region1("place name1")
+			.region2("place name2")
+			.region3("place name3")
+			.region4("place name4")
+			.placeName("place name")
+			.placeTime(LocalDateTime.now())
+			.placeCategory(PlaceCategoryType.ECT)
+			.journey(saveJourney)
+			.placeHeartCount(0)
+			.build());
+
+		placeImageRepository.save(PlaceImage.builder()
+			.place(savePlace)
+			.imageUrl("image url test1")
+			.build());
+
+		placeImageRepository.save(PlaceImage.builder()
+			.place(savePlace)
+			.imageUrl("image url test2")
+			.build());
+
+		List<MultipartFile> multipartFile = new ArrayList<>();
+		for (int i = 1; i <= 3; i++) {
+			String file = String.format("%d.png", i);
+			FileInputStream fis = new FileInputStream("src/main/resources/testImages/" + file);
+			multipartFile.add(new MockMultipartFile(String.format("%d", i), file, "png", fis));
+		}
+
+		//when
+		UpdateRequest updateRequest = UpdateRequest.builder()
+			.placeId(savePlace.getId())
+			.latitude(1.0)
+			.longitude(1.0)
+			.title("title test update")
+			.text("text test update")
+			.addressName("total address update")
+			.region1("place name1 update")
+			.region2("place name2 update")
+			.region3("place name3 update")
+			.region4("place name4 update")
+			.placeName("place name update")
+			.placeTime(LocalDateTime.now().minusDays(1))
+			.placeCategory("자연")
+			.journeyId(saveJourney.getId())
+			.build();
+
+		Response response = placeService.updatePlace(multipartFile, updateRequest);
+
+		//then
+		assertEquals(response.getImageUrls().size(), 3);
+		assertEquals(response.getPlaceId(), savePlace.getId());
+		assertEquals(response.getText(), updateRequest.getText());
+		assertEquals(response.getTitle(), updateRequest.getTitle());
+		assertEquals(response.getAddressName(), updateRequest.getAddressName());
+		assertEquals(response.getRegion1(), updateRequest.getRegion1());
+		assertEquals(response.getRegion2(), updateRequest.getRegion2());
+		assertEquals(response.getRegion3(), updateRequest.getRegion3());
+		assertEquals(response.getRegion4(), updateRequest.getRegion4());
+		assertEquals(response.getPlaceName(), updateRequest.getPlaceName());
+		assertEquals(response.getPlaceCategory(), updateRequest.getPlaceCategory());
+		assertEquals(response.getJourneyId(), saveJourney.getId());
+
+		List<String> imageUrls = response.getImageUrls();
+		for (String url : imageUrls) {
+			awsS3Service.deleteFile(url);
+		}
+	}
+
+	@DisplayName("05_01. updatePlace fail not found place")
+	@Test
+	public void test_05_01() throws IOException {
+		//given
+		Journey saveJourney = journeyRepository.save(Journey.builder().build());
+
+		Place savePlace = Place.builder().id(1000000L).build();
+
+		List<MultipartFile> multipartFile = new ArrayList<>();
+		for (int i = 1; i <= 3; i++) {
+			String file = String.format("%d.png", i);
+			FileInputStream fis = new FileInputStream("src/main/resources/testImages/" + file);
+			multipartFile.add(new MockMultipartFile(String.format("%d", i), file, "png", fis));
+		}
+
+		//when
+		UpdateRequest updateRequest = UpdateRequest.builder()
+			.placeId(savePlace.getId())
+			.latitude(1.0)
+			.longitude(1.0)
+			.title("title test update")
+			.text("text test update")
+			.addressName("total address update")
+			.region1("place name1 update")
+			.region2("place name2 update")
+			.region3("place name3 update")
+			.region4("place name4 update")
+			.placeName("place name update")
+			.placeTime(LocalDateTime.now().minusDays(1))
+			.placeCategory("자연")
+			.journeyId(saveJourney.getId())
+			.build();
+
+		PlaceException placeException = assertThrows(PlaceException.class,
+			() -> placeService.updatePlace(multipartFile, updateRequest));
+
+		//then
+		assertEquals(placeException.getErrorCode(), ErrorCode.NOT_FOUND_PLACE);
+	}
+
+	@DisplayName("05_02. updatePlace fail category mismatch ")
+	@Test
+	public void test_05_02() throws IOException {
+		//given
+		Journey saveJourney = journeyRepository.save(Journey.builder().build());
+
+		Place savePlace =
+			placeRepository.save(Place.builder()
+				.latitude(1.0)
+				.longitude(1.0)
+				.title("title test")
+				.text("text test")
+				.addressName("total address")
+				.region1("place name1")
+				.region2("place name2")
+				.region3("place name3")
+				.region4("place name4")
+				.placeName("place name")
+				.placeTime(LocalDateTime.now())
+				.placeCategory(PlaceCategoryType.ECT)
+				.journey(saveJourney)
+				.placeHeartCount(0)
+				.build());
+
+		placeImageRepository.save(PlaceImage.builder()
+			.place(savePlace)
+			.imageUrl("image url test1")
+			.build());
+
+		placeImageRepository.save(PlaceImage.builder()
+			.place(savePlace)
+			.imageUrl("image url test2")
+			.build());
+
+		List<MultipartFile> multipartFile = new ArrayList<>();
+		for (int i = 1; i <= 3; i++) {
+			String file = String.format("%d.png", i);
+			FileInputStream fis = new FileInputStream("src/main/resources/testImages/" + file);
+			multipartFile.add(new MockMultipartFile(String.format("%d", i), file, "png", fis));
+		}
+
+		//when
+		UpdateRequest updateRequest = UpdateRequest.builder()
+			.placeId(savePlace.getId())
+			.latitude(1.0)
+			.longitude(1.0)
+			.title("title test update")
+			.text("text test update")
+			.addressName("total address update")
+			.region1("place name1 update")
+			.region2("place name2 update")
+			.region3("place name3 update")
+			.region4("place name4 update")
+			.placeName("place name update")
+			.placeTime(LocalDateTime.now().minusDays(1))
+			.placeCategory("자연dddd")
+			.journeyId(saveJourney.getId())
+			.build();
+
+		PlaceException placeException = assertThrows(PlaceException.class,
+			() -> placeService.updatePlace(multipartFile, updateRequest));
+
+		//then
+		assertEquals(placeException.getErrorCode(), ErrorCode.MISMATCH_PLACE_CATEGORY_TYPE);
 	}
 }
