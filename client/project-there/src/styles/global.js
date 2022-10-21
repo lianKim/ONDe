ac6f8930e36ee9e35fc4e40d1c68fc2d7c72291d
lianKim <5ffcut@gmail.com>
2022-10-21 19:48:22 +0900

    Design : 글로벌 스타일 수정

--- client/project-there/src/styles/global.js
+++ client/project-there/src/styles/global.js
@@ -1,6 +1,29 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+
+
+  @font-face {
+    font-family: 'Poppins', sans-serif;
+    src: url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'))
+      format("woff");
+    font-weight: normal;
+    font-style: normal;
+    unicode-range: U+0041-005A, U+0061-007A;
+  }
+
+  @font-face {
+    font-family: 'Noto Sans KR', sans-serif;
+    src: url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap')
+      format("woff");
+    font-weight: normal;
+    font-style: normal;
+    letter-spacing: -0.5px;
+    unicode-range: U+AC00-U+D7A3;
+  }
+
   *, *::before, *::after {
     box-sizing: border-box;
   }
@@ -8,9 +31,10 @@ const GlobalStyle = createGlobalStyle`
   html {
     height: 100%;
   }
-
+ㄴ
   body {
     height: 100%;
+    font-family: 'Poppins', 'Noto Sans KR', sans-serif;
   }
 
   a {
