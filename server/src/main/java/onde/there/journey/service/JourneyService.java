00035d846af3fb0b7edf06323094f137f47d520b
Juwon <tjwndnjs1998@gmail.com>
2022-10-25 18:29:57 +0900

    Refactor : JourneyService 수정 각 메소드별 호출 및 종료 등 log 추가

--- server/src/main/java/onde/there/journey/service/JourneyService.java
+++ server/src/main/java/onde/there/journey/service/JourneyService.java
@@ -46,13 +46,14 @@ public class JourneyService {
 
 	@Transactional
 	public JourneyDto.CreateResponse createJourney(
-		JourneyDto.CreateRequest request,
-		MultipartFile thumbnail) {
+		JourneyDto.CreateRequest request, MultipartFile thumbnail) {
+
+		log.info("createJourney() : 호출");
 
 		Member member = new Member("test", "test", "1", "1");
 		memberRepository.save(member);
 
-//		Member member = memberRepository.findByEmail(request.getMemberEmail())
+//		Member member = memberRepository.findById(request.getMemberId())
 //			.orElseThrow(() -> new JourneyException(NOT_FOUND_MEMBER));
 
 		if (request.getEndDate().isBefore(request.getStartDate())) {
@@ -62,7 +63,7 @@ public class JourneyService {
 		List<String> imageUrls = awsS3Service.uploadFiles(
 			Collections.singletonList(thumbnail));
 
-		log.info("여정 thumbnail 업로드 완료 "
+		log.info("createJourney() : 여정 thumbnail 업로드 완료, "
 			+ "(여정 thumbnail URL : " + imageUrls.get(0) + ")");
 
 		Journey journey = Journey.builder()
@@ -78,7 +79,7 @@ public class JourneyService {
 			.build();
 
 		journeyRepository.save(journey);
-		log.info("journey 생성 완료");
+		log.info("createJourney() : 여정 생성 완료,  JourneyId : " + journey.getId());
 
 		List<String> inputJourneyThemes = request.getJourneyThemes();
 		for (String inputJourneyTheme : inputJourneyThemes) {
@@ -88,24 +89,32 @@ public class JourneyService {
 					JourneyThemeType.findByTheme(inputJourneyTheme))
 				.build();
 			journeyThemeRepository.save(journeyTheme);
-			log.info("journeyTheme 생성 완료");
+			log.info(
+				"createJourney() : journeyTheme 생성 완료, journeyThemeId : "
+					+ journeyTheme.getId());
 		}
 
+		log.info("createJourney() : 종료");
 		return JourneyDto.CreateResponse.fromEntity(journey,
 			inputJourneyThemes);
 	}
 
 	public List<JourneyDto.JourneyListResponse> list() {
 
+		log.info("list() : 호출");
+
 		List<JourneyDto.JourneyListResponse> list = new ArrayList<>();
 		List<Journey> journeyList = journeyRepository
 			.findAllByDisclosure("public");
-		log.info("JourneyList 조회 완료");
+
+		log.info("list() : 종료");
 
 		return getList(list, journeyList);
 	}
 
 	public List<JourneyDto.JourneyListResponse> myList(String memberId) {
+
+		log.info("myList() : 호출");
 
 		Member member = memberRepository.findById(memberId)
 			.orElseThrow(() -> new JourneyException(NOT_FOUND_MEMBER));
@@ -113,7 +122,8 @@ public class JourneyService {
 		List<JourneyDto.JourneyListResponse> list = new ArrayList<>();
 		List<Journey> journeyList = journeyRepository
 			.findAllByMember(member);
-		log.info("MyJourneyList 조회 완료");
+
+		log.info("myList() : 조회 완료");
 
 		return getList(list, journeyList);
 	}
@@ -121,11 +131,16 @@ public class JourneyService {
 	public List<Journey> filteredList(
 		JourneySearchTheme journeySearchTheme) {
 
+		log.info("filteredList() : 호출");
+
+		log.info("filteredList() : 종료");
 		return journeyRepositoryImpl.searchAll(journeySearchTheme);
 	}
 
 	private List<JourneyListResponse> getList(List<JourneyListResponse> list,
 		List<Journey> journeyList) {
+
+		log.info("getList() : 호출");
 
 		for (Journey journey : journeyList) {
 			List<String> journeyThemeTypeList = journeyThemeRepository
@@ -139,10 +154,14 @@ public class JourneyService {
 				JourneyListResponse.fromEntity(journey, journeyThemeTypeList));
 		}
 
+		log.info("getList() : 종료");
+
 		return list;
 	}
 
 	public DetailResponse journeyDetail(Long journeyId) {
+
+		log.info("journeyDetail() : 호출");
 
 		Journey journey = journeyRepository.findById(journeyId)
 			.orElseThrow(() -> new JourneyException(NOT_FOUND_JOURNEY));
@@ -153,12 +172,18 @@ public class JourneyService {
 				.getJourneyThemeName()
 				.getThemeName())
 			.collect(Collectors.toList());
+		log.info(
+			"journeyDetail() : journey 조회 완료, journeyId : " + journey.getId());
+
+		log.info("journeyDetail() : 종료");
 
 		return DetailResponse.fromEntity(journey, journeyThemeTypeList);
 	}
 
 	@Transactional
 	public void deleteJourney(Long journeyId) {
+
+		log.info("deleteJourney() : 호출");
 
 		Journey journey = journeyRepository.findById(journeyId)
 			.orElseThrow(() -> new JourneyException(NOT_FOUND_JOURNEY));
@@ -170,7 +195,8 @@ public class JourneyService {
 		journeyThemeRepository.deleteAll(journeyThemeTypeList);
 		journeyRepository.delete(journey);
 
-		log.info("여정 삭제 완료 journeyId : " + journey.getId());
+		log.info("deleteJourney() : 여정 삭제 완료, journeyId : " + journey.getId());
+		log.info("deleteJourney() : 종료");
 
 	}
 
@@ -178,11 +204,13 @@ public class JourneyService {
 	public UpdateResponse updateJourney(UpdateRequest request,
 		MultipartFile thumbnail) {
 
+		log.info("updateJourney() : 호출");
+
 		Journey journey = journeyRepository.findById(request.getJourneyId())
 			.orElseThrow(() -> new JourneyException(NOT_FOUND_JOURNEY));
 
 		String email = journey.getMember().getEmail();
-		if (!Objects.equals(email, request.getMemberEmail())) {
+		if (!Objects.equals(email, request.getMemberId())) {
 			throw new JourneyException(YOU_ARE_NOT_THE_AUTHOR);
 		}
 
@@ -202,7 +230,7 @@ public class JourneyService {
 					JourneyThemeType.findByTheme(inputJourneyTheme))
 				.build();
 			journeyThemeRepository.save(journeyTheme);
-			log.info("journeyTheme 수정 완료");
+			log.info("updateJourney() : journeyTheme 수정 완료");
 		}
 
 		journey.setTitle(request.getTitle());
@@ -213,7 +241,10 @@ public class JourneyService {
 		journey.setIntroductionText(request.getIntroductionText());
 		journey.setJourneyThumbnailUrl(imageUrls.get(0));
 		journey.setRegion(findByRegion(request.getRegion()));
-		log.info("여정 Entity 수정 완료 journeyId : " + journey.getId());
+		log.info("updateJourney() : journey 수정 완료, journeyId : "
+			+ journey.getId());
+
+		log.info("updateJourney() : 종료");
 
 		return JourneyDto.UpdateResponse
 			.fromEntity(journey, inputJourneyThemes);
