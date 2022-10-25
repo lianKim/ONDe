2fd1349525361db8700d8a49e75a9425ab4c9d30
Juwon <tjwndnjs1998@gmail.com>
2022-10-25 18:27:42 +0900

    Refactor : JourneyDto 수정 @Schema annotation 추가, @NotNull annotation 추가

--- server/src/main/java/onde/there/dto/journy/JourneyDto.java
+++ server/src/main/java/onde/there/dto/journy/JourneyDto.java
@@ -1,5 +1,6 @@
 package onde.there.dto.journy;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
 import java.util.List;
 import javax.validation.constraints.Min;
@@ -16,6 +17,7 @@ import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 @Getter
 @Setter
+@Schema
 public class JourneyDto {
 
 	@Setter
@@ -23,32 +25,44 @@ public class JourneyDto {
 	@AllArgsConstructor
 	@NoArgsConstructor
 	@Builder
+	@Schema(name = "여정 생성 요청 parameter")
 	public static class CreateRequest {
 
 		@NotBlank(message = "이메일을 입력해주세요.")
-		private String memberEmail;
+		@Schema(description = "유저 아이디")
+		private String memberId;
 
 		@NotBlank(message = "제목을 입력해주세요.")
+		@Schema(description = "여정 제목")
 		private String title;
 
 		@DateTimeFormat(iso = ISO.DATE)
+		@Schema(description = "여정 시작 날짜")
 		private LocalDate startDate;
 
 		@DateTimeFormat(iso = ISO.DATE)
+		@Schema(description = "여정 종료 날짜")
 		private LocalDate endDate;
 
 		@NotNull
 		@Min(1)
+		@Schema(description = "인원")
 		private int numberOfPeople;
 
 		@NotBlank(message = "공개범위를 선택해주세요.")
+		@Schema(description = "공개 범위")
 		private String disclosure;
 
 		@NotNull
+		@Schema(description = "여정 테마")
 		private List<String> journeyThemes;
 
+		@NotNull
+		@Schema(description = "소개 글")
 		private String introductionText;
 
+		@NotNull
+		@Schema(description = "지역")
 		private String region;
 
 
@@ -136,34 +150,48 @@ public class JourneyDto {
 	@AllArgsConstructor
 	@NoArgsConstructor
 	@Builder
+	@Schema(name = "여정 수정 요청 parameter")
 	public static class UpdateRequest {
 
+		@NotBlank
+		@Schema(description = "여정 아이디")
 		private Long journeyId;
 
 		@NotBlank(message = "이메일을 입력해주세요.")
-		private String memberEmail;
+		@Schema(description = "유저 아이디")
+		private String memberId;
 
 		@NotBlank(message = "제목을 입력해주세요.")
+		@Schema(description = "여정 제목")
 		private String title;
 
 		@DateTimeFormat(iso = ISO.DATE)
+		@Schema(description = "여정 시작 날짜")
 		private LocalDate startDate;
 
 		@DateTimeFormat(iso = ISO.DATE)
+		@Schema(description = "여정 종료 날짜")
 		private LocalDate endDate;
 
 		@NotNull
 		@Min(1)
+		@Schema(description = "인원")
 		private int numberOfPeople;
 
 		@NotBlank(message = "공개범위를 선택해주세요.")
+		@Schema(description = "공개 범위")
 		private String disclosure;
 
 		@NotNull
+		@Schema(description = "여정 테마")
 		private List<String> journeyThemes;
 
+		@NotNull
+		@Schema(description = "소개 글")
 		private String introductionText;
 
+		@NotNull
+		@Schema(description = "지역")
 		private String region;
 
 
