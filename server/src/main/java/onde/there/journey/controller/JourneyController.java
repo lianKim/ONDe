58787d45bf666d808f48cadae80039c45840939f
Juwon <tjwndnjs1998@gmail.com>
2022-10-25 18:28:59 +0900

    Refactor : JourneyController 수정 swagger v3 관련 annotation 추가

--- server/src/main/java/onde/there/journey/controller/JourneyController.java
+++ server/src/main/java/onde/there/journey/controller/JourneyController.java
@@ -1,5 +1,10 @@
 package onde.there.journey.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -29,28 +34,44 @@ public class JourneyController {
 
 	private final JourneyService journeyService;
 
+	@Operation(summary = "여정 생성",
+		description = "여정을 생성하고, 생성된 여정에 입력받은 여정 테마를 생성")
+	@ApiResponse(responseCode = "200", description = "생성된 여정 반환",
+		content = @Content(schema = @Schema(implementation = JourneyDto.CreateResponse.class)))
 	@PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE,
 		MediaType.APPLICATION_JSON_VALUE})
-	public ResponseEntity<?> createJourney(
+	public ResponseEntity<JourneyDto.CreateResponse> createJourney(
+		@Parameter(name = "여정 정보", description = "생성할 여정에 대한 정보", required = true,
+			content = @Content(schema = @Schema(implementation = JourneyDto.CreateRequest.class)))
 		@RequestPart @Valid JourneyDto.CreateRequest request,
+		@Parameter(description = "Thumbnail 이미지 파일", required = true)
 		@RequestPart MultipartFile thumbnail) {
 
 		return ResponseEntity.ok(
 			journeyService.createJourney(request, thumbnail));
 	}
 
+	@Operation(summary = "여정 수정", description = "여정을 수정합니다.")
+	@ApiResponse(responseCode = "200", description = "수정된 여정 반환",
+		content = @Content(schema = @Schema(implementation = JourneyDto.UpdateResponse.class)))
 	@PatchMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE,
 		MediaType.APPLICATION_JSON_VALUE})
-	public ResponseEntity<?> updateJourney(
+	public ResponseEntity<JourneyDto.UpdateResponse> updateJourney(
+		@Parameter(name = "여정 정보", description = "수정할 여정에 대한 정보", required = true,
+			content = @Content(schema = @Schema(implementation = JourneyDto.UpdateRequest.class)))
 		@RequestPart @Valid JourneyDto.UpdateRequest request,
+		@Parameter(description = "Thumbnail 이미지 파일", required = true)
 		@RequestPart MultipartFile thumbnail) {
 
 		return ResponseEntity.ok(
 			journeyService.updateJourney(request, thumbnail));
 	}
 
+	@Operation(summary = "여정 삭제", description = "여정을 삭제합니다.")
+	@ApiResponse(responseCode = "200", description = "삭제된 여정 id 반환")
 	@DeleteMapping
-	public ResponseEntity<?> deleteJourney(
+	public ResponseEntity<String> deleteJourney(
+		@Parameter(description = "삭제할 여정 id", required = true)
 		@RequestParam Long journeyId) {
 
 		journeyService.deleteJourney(journeyId);
@@ -58,27 +79,39 @@ public class JourneyController {
 		return ResponseEntity.ok("journeyId : " + journeyId);
 	}
 
+	@Operation(summary = "여정 조회", description = "여정을 조회합니다.")
+	@ApiResponse(responseCode = "200", description = "조회한 여정을 반환",
+		content = @Content(schema = @Schema(implementation = JourneyDto.DetailResponse.class)))
 	@GetMapping("/detail")
 	public ResponseEntity<DetailResponse> getJourneyDetail(
+		@Parameter(description = "조회할 여정 id", required = true)
 		@RequestParam Long journeyId
 	) {
 
 		return ResponseEntity.ok(journeyService.journeyDetail(journeyId));
 	}
 
+	@Operation(summary = "모든 여정 조회", description = "모든 여정을 조회합니다.")
+	@ApiResponse(responseCode = "200", description = "모든 여정을 반환",
+		content = @Content(schema = @Schema(implementation = JourneyDto.JourneyListResponse.class)))
 	@GetMapping("/list")
 	public ResponseEntity<List<JourneyListResponse>> getJourneyList() {
 
 		return ResponseEntity.ok(journeyService.list());
 	}
 
+	@Operation(summary = "내 여정 조회", description = "내 여정을 조회합니다.")
+	@ApiResponse(responseCode = "200", description = "내 여정을 반환",
+		content = @Content(schema = @Schema(implementation = JourneyDto.JourneyListResponse.class)))
 	@GetMapping("/my-list")
 	public ResponseEntity<List<JourneyListResponse>> getMyJourneyList(
+		@Parameter(description = "내 아이디", required = true)
 		@RequestParam String memberId) {
 
 		return ResponseEntity.ok(journeyService.myList(memberId));
 	}
 
+	@Operation(summary = "여정 필터링", description = "필터링된 여정을 조회합니다.")
 	@PostMapping("/filtered-list")
 	public ResponseEntity<List<Journey>> getFilteredList(
 		@RequestBody JourneySearchTheme journeySearchTheme) {
