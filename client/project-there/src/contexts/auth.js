5a8fd3180cebe8761e510237d904a1145a48b701
lianKim <5ffcut@gmail.com>
2022-11-04 12:44:22 +0900

    Fix : 여정 생성 에러 수정

--- client/project-there/src/contexts/auth.js
+++ client/project-there/src/contexts/auth.js
@@ -6,7 +6,6 @@ import React, {
   useMemo,
   useState,
 } from 'react';
-import { getAccessToken } from '../lib/utills/controlAccessToken';
 import { authAPI } from '../lib/utills/http';
 
 const AuthValueContext = createContext();
@@ -16,20 +15,29 @@ const initialState = {
   id: '',
   email: '',
   name: '',
+  nickname: '',
   profileImageUrl: '',
 };
 
 export default function AuthProvider({ children }) {
   const [userInfo, setUserInfo] = useState(initialState);
-  // const accessToken = getAccessToken();
 
   const actions = useMemo(() => ({
     // 유저 인증
     async authenticateUser(accessToken) {
       try {
-        const { id, email, name, profileImageUrl } = await authAPI(accessToken);
+        const { id, email, name, nickname, profileImageUrl } = await authAPI(
+          accessToken,
+        );
         console.log(`id: ${id}`);
-        setUserInfo((prev) => ({ ...prev, id, email, name, profileImageUrl }));
+        setUserInfo((prev) => ({
+          ...prev,
+          id,
+          email,
+          name,
+          nickname,
+          profileImageUrl,
+        }));
       } catch (err) {
         console.log(err);
       }
