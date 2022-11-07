5b8dbb37824c3ac039634d4319074f5ad8bf599a
roomdoor <roomdoor.lee@gmail.com>
2022-11-07 10:05:06 +0900

    Refactor : PlaceController swagger TokenMemberId
    sagger 문서에 TokenMemberId  API 매개변수로 나오는것 수정
    매개변수 줄 정리

--- server/src/main/java/onde/there/place/controller/PlaceController.java
+++ server/src/main/java/onde/there/place/controller/PlaceController.java
@@ -37,10 +37,13 @@ public class PlaceController {
 	@ApiResponse(responseCode = "201", description = "생성된 장소 반환", content = @Content(schema = @Schema(implementation = PlaceDto.Response.class)))
 	@PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
 	public ResponseEntity<PlaceDto.Response> createPlace(
-		@Parameter(description = "Image (여러 파일 업로드 가능)", required = true) @RequestPart List<MultipartFile> multipartFile,
+		@Parameter(description = "Image (여러 파일 업로드 가능)", required = true)
+		@RequestPart List<MultipartFile> multipartFile,
 		@Parameter(description = "장소 생성 정보", required = true,
 			content = @Content(schema = @Schema(implementation = PlaceDto.CreateRequest.class)))
-		@RequestPart @Valid PlaceDto.CreateRequest request, @TokenMemberId String memberId) {
+		@RequestPart @Valid PlaceDto.CreateRequest request,
+		@Parameter(hidden = true)
+		@TokenMemberId String memberId) {
 		return ResponseEntity.ok(
 			Response.toResponse(placeService.createPlace(multipartFile, request, memberId)));
 	}
@@ -49,7 +52,8 @@ public class PlaceController {
 	@ApiResponse(responseCode = "200", description = "조회한 장소 반환", content = @Content(schema = @Schema(implementation = PlaceDto.Response.class)))
 	@GetMapping()
 	public ResponseEntity<PlaceDto.Response> getPlace(
-		@Parameter(description = "장소 아이디", required = true) @RequestParam Long placeId) {
+		@Parameter(description = "장소 아이디", required = true)
+		@RequestParam Long placeId) {
 		return ResponseEntity.ok(placeService.getPlace(placeId));
 	}
 
@@ -57,7 +61,9 @@ public class PlaceController {
 	@ApiResponse(responseCode = "200", description = "조회한 장소 list 반환", content = @Content(schema = @Schema(implementation = PlaceDto.Response.class)))
 	@GetMapping("/list")
 	public ResponseEntity<List<Response>> list(
-		@Parameter(description = "여정 아이디", required = true) @RequestParam Long journeyId,
+		@Parameter(description = "여정 아이디", required = true)
+		@RequestParam Long journeyId,
+		@Parameter(hidden = true)
 		@TokenMemberId String memberId) {
 		return ResponseEntity.ok(placeService.list(journeyId, memberId));
 	}
@@ -65,7 +71,9 @@ public class PlaceController {
 	@Operation(summary = "장소 삭제", description = "placeId에 해당하는 장소 삭제")
 	@DeleteMapping()
 	public ResponseEntity<?> deletePlace(
-		@Parameter(description = "장소 아이디", required = true) @RequestParam Long placeId,
+		@Parameter(description = "장소 아이디", required = true)
+		@RequestParam Long placeId,
+		@Parameter(hidden = true)
 		@TokenMemberId String memberId) {
 		return ResponseEntity.ok(placeService.delete(placeId, memberId));
 	}
@@ -73,7 +81,9 @@ public class PlaceController {
 	@Operation(summary = "여정의 모든 장소 삭제", description = "journeyId에 해당하는 여정에 포함된 모든 장소 삭제")
 	@DeleteMapping("/delete-all")
 	public ResponseEntity<?> deleteAll(
-		@Parameter(description = "여정 아이디", required = true) @RequestParam Long journeyId,
+		@Parameter(description = "여정 아이디", required = true)
+		@RequestParam Long journeyId,
+		@Parameter(hidden = true)
 		@TokenMemberId String memberId) {
 		return ResponseEntity.ok(placeService.deleteAll(journeyId, memberId));
 	}
@@ -82,10 +92,13 @@ public class PlaceController {
 	@ApiResponse(responseCode = "200", description = "업데이트된 장소 반환", content = @Content(schema = @Schema(implementation = PlaceDto.Response.class)))
 	@PutMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
 	public ResponseEntity<PlaceDto.Response> updatePlace(
-		@Parameter(description = "Image (여러 파일 업로드 가능)", required = true) @RequestPart List<MultipartFile> multipartFile,
+		@Parameter(description = "Image (여러 파일 업로드 가능)", required = true)
+		@RequestPart List<MultipartFile> multipartFile,
 		@Parameter(description = "장소 업데이트 정보", required = true,
 			content = @Content(schema = @Schema(implementation = PlaceDto.UpdateRequest.class)))
-		@RequestPart @Valid PlaceDto.UpdateRequest request, @TokenMemberId String memberId) {
+		@RequestPart @Valid PlaceDto.UpdateRequest request,
+		@Parameter(hidden = true)
+		@TokenMemberId String memberId) {
 		return ResponseEntity.ok(placeService.updatePlace(multipartFile, request, memberId));
 	}
 }
