02c3bc3a7448d1e7df08689e112becfbc1b0f7f6
lianKim <5ffcut@gmail.com>
2022-11-14 16:47:29 +0900

    Refactor : 페이지별 회원 인증하는 코드 삭제

--- client/project-there/src/pages/JourneyUploadPage.jsx
+++ client/project-there/src/pages/JourneyUploadPage.jsx
@@ -1,22 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import JourneyUploadContainer from '../components/journeyUpload/JourneyUploadContainer';
-import { useAuthActions } from '../contexts/AuthContext';
 import NewJourneyProvider from '../contexts/NewJourneyContext';
-import { getAccessToken } from '../lib/utills/controlAccessToken';
 
 function JourneyUploadPage() {
-  // 유저 인증
-  const { authenticateUser, initUserInfo } = useAuthActions();
-
-  useEffect(() => {
-    // 마운트 될 때 accessToken으로 받은 유저 정보 업데이트
-    const accessToken = getAccessToken();
-    if (accessToken) {
-      console.log(accessToken);
-      authenticateUser(accessToken);
-    }
-  }, []);
-
   return (
     <NewJourneyProvider>
       <JourneyUploadContainer />
